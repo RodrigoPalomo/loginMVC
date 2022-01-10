114aa5e60119039bec542803c21674d554bb070e
Rodrigo Palomo <repmlm@hotmail.com>
2022-01-10 18:55:36 -0300

    Agregar archivos de proyecto.

---
+++ cursomvc/Controllers/AccesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cursomvc.Controllers
+{
+    public class AccesController : Controller
+    {
+        // GET: Acces
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string user, string password)
+        {
+            try
+            {
+                return Content("1");   
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrió un error :( " + ex.Message);
+            }
+        }
+    }
+}
